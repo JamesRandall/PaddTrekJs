b111396b5793753d2bc893b083b24a170ee02520
James Randall <james@accidentalfish.com>
2011-04-17 11:13:46 +0100

    License added, readme updated, removed redundant scripts, added browser check

--- scripts/main.js
+++ scripts/main.js
@@ -5,23 +5,31 @@ require(["jquery",
 		"extensions/jsExtensions",
 		"extensions/htmlExtensions",
 		"extensions/paddTrekExtensions"], function($,metrics,modelFactory,shortRangeControllerFactory,jsExtensions,htmlExtensions, paddTrekExtensions) {
-	jsExtensions();
-	htmlExtensions();
-	paddTrekExtensions();
+	if (Modernizr.canvastext)
+	{
+		jsExtensions();
+		htmlExtensions();
+		paddTrekExtensions();
 	
-    $(document).ready(function() {
-		var model;
-		var shortRangeController;
+	    $(document).ready(function() {
+			var model;
+			var shortRangeController;
 	
-		$("#paddTrekContainer").append(
-			$('<div>')
-				.addClass("game")
-				.append($('<canvas id="gameCanvas" width="' + metrics.gameWidth + '" height="' + metrics.gameHeight + '">'))
-				.appendTo($("#paddTrekContainer"))
-			);
+			$("#paddTrekContainer").append(
+				$('<div>')
+					.addClass("game")
+					.append($('<canvas id="gameCanvas" width="' + metrics.gameWidth + '" height="' + metrics.gameHeight + '">'))
+					.appendTo($("#paddTrekContainer"))
+				);
 		
-		model = modelFactory();
-		shortRangeController = shortRangeControllerFactory.create(model);
-		shortRangeController.activate();
-    });
+			model = modelFactory();
+			shortRangeController = shortRangeControllerFactory.create(model);
+			shortRangeController.activate();
+	    });
+	}
+	else
+	{
+		$("#browserNotSupported").show();
+		$("#paddTrekContainer").hide();
+	}
 });
